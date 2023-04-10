eb34ba50a9df7d868d4a4435290483c73fae9a36
Joe Walters <info@thenakeddeveloper.com>
2023-04-10 17:08:15 +0100

    Child and parent filter

--- test/Examples.Linq.Tests/ChildSelectTests.cs
+++ test/Examples.Linq.Tests/ChildSelectTests.cs
@@ -67,5 +67,29 @@ namespace Examples.Linq.Tests
             resultsFromMethodType.Should().HaveCount(data.JanuaryChildObjects); // Should be X items in the array for just January, the next test will determine the actual data
             resultsFromMethodType.Should().BeEquivalentTo(resultsFromQueryType); // The data should look the same in both methods (Also does the count)
         }
+
+        [Fact]
+        public void Get_Children_With_Child_And_Parent_Filter_Returns_Appropriate_Children()
+        {
+            // ARRANGE
+            TestData data = new TestData();
+
+            // ACT
+            List<Level2Object> resultsFromMethodType = data.Objects.Where(parent => parent.CreatedOn.Month == 1).SelectMany(c => c.Children).Where(c => c.CreatedOn.Month == 1).ToList();
+
+            var query = from o in data.Objects
+                        from c in o.Children
+                        where 
+                                o.CreatedOn.Month == 1
+                            && 
+                                c.CreatedOn.Month == 1
+                        select c;
+
+            List<Level2Object> resultsFromQueryType = query.ToList();
+
+            // ASSERT
+            resultsFromMethodType.Should().HaveCount(data.JanuaryChildObjects); // Should be X items in the array for just January, the next test will determine the actual data
+            resultsFromMethodType.Should().BeEquivalentTo(resultsFromQueryType); // The data should look the same in both methods (Also does the count)
+        }
     }
 }
