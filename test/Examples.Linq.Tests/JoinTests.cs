a66bd9caf310a2d1270215bebd9801d542c958f8
Joe Walters <info@thenakeddeveloper.com>
2023-04-11 22:44:05 +0100

    Empty Sort and Join test space

---
+++ test/Examples.Linq.Tests/JoinTests.cs
@@ -0,0 +1,19 @@
+using Examples.Linq.Objects;
+
+namespace Examples.Linq.Tests
+{
+    public class JoinTests
+    {
+        [Fact]
+        public void x()
+        {
+            // ARRANGE
+            TestData data = new TestData();
+
+            // ACT
+
+            // ASSERT
+        }
+
+    }
+}
---
+++ test/Examples.Linq.Tests/SortTests.cs
@@ -0,0 +1,18 @@
+using Examples.Linq.Objects;
+
+namespace Examples.Linq.Tests
+{
+    public class SortTests
+    {
+        [Fact]
+        public void x()
+        {
+            // ARRANGE
+            TestData data = new TestData();
+
+            // ACT
+
+            // ASSERT
+        }
+    }
+}
