46823a5837b43fb75738015b869107fe4dbfe250
Joe Walters <info@thenakeddeveloper.com>
2023-07-22 00:38:10 +0100

    Set up

--- test/Examples.Linq.Tests/JoinTests.cs
+++ test/Examples.Linq.Tests/JoinTests.cs
@@ -5,7 +5,18 @@ namespace Examples.Linq.Tests
     public class JoinTests
     {
         [Fact]
-        public void x()
+        public void Inner_Join()
+        {
+            // ARRANGE
+            TestData data = new TestData();
+
+            // ACT
+
+            // ASSERT
+        }
+
+        [Fact]
+        public void Outer_Join()
         {
             // ARRANGE
             TestData data = new TestData();
