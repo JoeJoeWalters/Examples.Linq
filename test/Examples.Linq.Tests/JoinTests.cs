7b67d36901b67091094c8e51c4c2043a133ee943
Joe Walters <info@thenakeddeveloper.com>
2023-07-29 23:34:53 +0100

    Join Tests

--- test/Examples.Linq.Tests/JoinTests.cs
+++ test/Examples.Linq.Tests/JoinTests.cs
@@ -11,20 +11,30 @@ namespace Examples.Linq.Tests
             TestData data = new TestData();
 
             // ACT
+            var resultsFromMethodType = data.People.Join(data.Pets,
+                                                    person => person,
+                                                    pet => pet.Owner,
+                                                    (person, pet) =>
+                                                        new
+                                                        {
+                                                            OwnerName = person.FirstName,
+                                                            PetName = pet.Name
+                                                        })
+                                                    .ToList();
+            var query =
+                from person in data.People
+                join pet in data.Pets on person equals pet.Owner
+                select new
+                {
+                    OwnerName = person.FirstName,
+                    PetName = pet.Name
+                };
+
+            var resultsFromQueryType = query.ToList();
 
             // ASSERT
+            resultsFromQueryType.Should().HaveCount(data.Pets.Count);
+            resultsFromMethodType.Should().BeEquivalentTo(resultsFromQueryType); // The data should look the same in both methods (Also does the count)
         }
-
-        [Fact]
-        public void Outer_Join()
-        {
-            // ARRANGE
-            TestData data = new TestData();
-
-            // ACT
-
-            // ASSERT
-        }
-
     }
 }
--- test/Examples.Linq.Tests/TestData.cs
+++ test/Examples.Linq.Tests/TestData.cs
@@ -1,12 +1,25 @@
 ﻿using Examples.Linq.Objects;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Examples.Linq.Tests
 {
+    public class Person
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+        public Person() { }
+        public Person(string firstName, string lastName) {  FirstName = firstName; LastName = lastName;}
+    }
+
+    public class Pet
+    {
+        public string Name { get; set; } = string.Empty;
+        public Person? Owner { get; set; }
+
+        public Pet() { }
+        public Pet(string name, Person owner) {  Name = name; Owner = owner; }
+    }
+
     public class TestData
     {
         public List<Level1Object> Objects = new List<Level1Object>();
@@ -16,8 +29,26 @@ namespace Examples.Linq.Tests
         public readonly int TotalAuthors = 2;
         public readonly int AuthorOccourances = 4;
 
+        static Person magnus = new(firstName: "Magnus", lastName: "Hedlund");
+        static Person terry = new(firstName: "Terry", lastName: "Adams");
+        static Person charlotte = new(firstName: "Charlotte", lastName: "Weiss");
+        static Person arlene = new(firstName: "Arlene", lastName: "Huff");
+        static Person rui = new(firstName: "Rui", lastName: "Raposo");
+
+        public List<Person> People = new() { magnus, terry, charlotte, arlene, rui };
+            
+        public List<Pet> Pets = new()
+            {
+                new(name: "Barley", owner: terry),
+                new(name: "Boots", owner: terry),
+                new(name: "Whiskers", owner: charlotte),
+                new(name: "Blue Moon", owner: rui),
+                new(name: "Daisy", owner: magnus),
+            };
+
         public TestData() 
         {
+
             Objects = new List<Level1Object>()
             {
                 new Level1Object()
